2f328a808c181a7b7f94a1f2e218269ec4c98164
NilanjanJana16 <nildbs16@gmail.com>
2022-01-04 20:26:31 +0530

    first commit

---
+++ pattern6.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class pattern6{
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int star = n/2 +1;
+        int space = 1;
+
+        for( int i = 1; i<= n; i++){
+
+            for( int j= 1; j<= star; j++){
+                System.out.print("*\t");
+            }
+
+            for( int j = 1; j<= space; j++){
+                System.out.print("\t");
+            }
+
+            for(int j = 1; j<= star; j++){
+                System.out.print("*\t");
+            }
+
+            if(i<=n/2){
+                star--;
+                space += 2;
+            }else{
+                star++;
+                space -= 2;
+            }
+
+            System.out.println();
+
+        }
+
+    
+    }
+}
